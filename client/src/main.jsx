2b89e1cd88e8048638860e0a6000b0b21447c158
Nicolás <nicoo.marsili@gmail.com>
2023-02-14 18:04:40 -0300

    Env variables - Commerce Installation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,10 +9,6 @@ import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
